cd1c4f5b2cefc5885572d74018365a751f13dc4c
samugs13 <garciasanchezsamu@gmail.com>
2022-01-04 23:36:44 +0100

    modified home component labels

--- components/home/home.component.js
+++ components/home/home.component.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import '../../css/home.css';
-import { LangContext } from "../lang/LangContext.component";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class Home extends Component {
 	render() {
 		return (
-			<div className="home">
-				<br></br>
-				<LangContext.Consumer>
-					{(context) => {
-						return <div>
-							<h1 className="titulo">{context.dictionary["title"]}</h1>
-							<p className="texto">{context.dictionary["text"]}</p>
-						</div>
-					}
-					}
-				</LangContext.Consumer>
-			</div>
+			<View>
+				<Text>JUEGOS PARA TODOS</Text>
+				<Text>La mejor página de minijuegos</Text>
+			</View>
 		);
 	}
 }
