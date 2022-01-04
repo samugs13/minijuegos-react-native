ebf967bae3281da680f768b4e6118a60f4843ffd
samugs13 <garciasanchezsamu@gmail.com>
2022-01-04 23:52:10 +0100

    modified navbar component labels

--- components/navbar/Navbar.component.js
+++ components/navbar/Navbar.component.js
@@ -1,32 +1,18 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { LangContext } from "../lang/LangContext.component";
-import LangSelector from "../lang/LangSelector.component";
+import Home from '../home/home.component'
+
+const Stack = createStackNavigator();
 
 export default function Navbar() {
 
-    const lang = useContext(LangContext);
-
-	return (
-		<nav className="navbar navbar-expand navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">{lang.dictionary["title"]}</a>
-                <div className="navbar-nav me-auto">
-                    <li className="nav-item">
-                        <Link to={"/"} className="nav-link">{lang.dictionary["home"]}</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/quiz"} className="nav-link">{lang.dictionary["quiz"]}</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/tictactoe"} className="nav-link">{lang.dictionary["tictactoe"]}</Link>
-                    </li>
-                </div>
-                <div className="d-flex">
-                    <LangSelector />
-                </div>
-            </div>
-		</nav>
-	);
+	return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
